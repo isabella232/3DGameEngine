e9626b90fe9a135255e6ac654a47c82558f5dc71
ElgarL <ElgarL@ElgarL-PC>
2015-05-28 08:14:45 +0100

    Calculate Tangents on the Mesh.

--- src/com/base/engine/rendering/Mesh.java
+++ src/com/base/engine/rendering/Mesh.java
@@ -37,6 +37,8 @@ public class Mesh extends MeshResource {
 		if (calcNormals) {
 			calcNormals(vertices, indices);
 		}
+		
+		CalcTangents(vertices, indices);
 
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, getVbo());
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, Util.createFlippedBuffer(vertices), GL15.GL_STATIC_DRAW);
@@ -85,6 +87,38 @@ public class Mesh extends MeshResource {
 		for (final Vertex vertice : vertices) {
 			vertice.setNormal(vertice.getNormal().normalized());
 		}
+	}
+	
+	public void CalcTangents(final Vertex[] vertices, final int[] indices) {
+		for (int i = 0; i < indices.length; i += 3) {
+			int i0 = indices[i];
+			int i1 = indices[i + 1];
+			int i2 = indices[i + 2];
+
+			Vector3f edge1 = vertices[i1].getPos().sub(vertices[i0].getPos());
+			Vector3f edge2 = vertices[i2].getPos().sub(vertices[i0].getPos());
+
+			float deltaU1 = vertices[i1].getTexCoord().getX() - vertices[i0].getTexCoord().getX();
+			float deltaV1 = vertices[i1].getTexCoord().getY() - vertices[i0].getTexCoord().getY();
+			float deltaU2 = vertices[i2].getTexCoord().getX() - vertices[i0].getTexCoord().getX();
+			float deltaV2 = vertices[i2].getTexCoord().getY() - vertices[i0].getTexCoord().getY();
+
+			float dividend = (deltaU1 * deltaV2 - deltaU2 * deltaV1);
+			// TODO: The first 0.0f may need to be changed to 1.0f here.
+			float f = dividend == 0 ? 0.0f : 1.0f / dividend;
+
+			Vector3f tangent = new Vector3f(0, 0, 0);
+			tangent.setX(f * (deltaV2 * edge1.getX() - deltaV1 * edge2.getX()));
+			tangent.setY(f * (deltaV2 * edge1.getY() - deltaV1 * edge2.getY()));
+			tangent.setZ(f * (deltaV2 * edge1.getZ() - deltaV1 * edge2.getZ()));
+
+			vertices[i0].getTangent().set(vertices[i0].getTangent().add(tangent));
+			vertices[i1].getTangent().set(vertices[i1].getTangent().add(tangent));
+			vertices[i2].getTangent().set(vertices[i2].getTangent().add(tangent));
+		}
+
+		for (int i = 0; i < vertices.length; i++)
+			vertices[i].getTangent().set(vertices[i].getTangent().normalized());
 	}
 
 }
